0d3e5cde760d64f86f4451bcda3c2c11e1c8d08c
CFSC13 <christiansanabriapiano@gmail.com>
2023-09-24 07:52:44 -0300

    botones_pdf_excel_en_listados

--- modulos/productos.php
+++ modulos/productos.php
@@ -424,10 +424,23 @@ if($_GET[del]!="")
     //fin editor
 </script>
 
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/buttons/2.4.1/css/buttons.dataTables.min.css">
+
+<script type="text/javascript" language="javascript" src="https://cdn.datatables.net/buttons/2.4.1/js/dataTables.buttons.min.js"></script>
+ <script type="text/javascript" language="javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js"></script>
+ <script type="text/javascript" language="javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+ <script type="text/javascript" language="javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+ <script type="text/javascript" language="javascript" src="https://cdn.datatables.net/buttons/2.4.1/js/buttons.html5.min.js"></script>
+ <script type="text/javascript" language="javascript" src="https://cdn.datatables.net/buttons/2.4.1/js/buttons.print.min.js"></script>
 <script>
 $(document).ready( function () {
+    
     //inicio datatable
     $('#dataTable-mensajes').DataTable({
+        dom: 'Bfrtip',
+        buttons: [
+            'copy', 'csv', 'excel', 'pdf', 'print'
+        ],
         sort: true, 
         order : [[0,"desc"]],
         responsive: true,
@@ -455,6 +468,9 @@ $(document).ready( function () {
 
     //inicializar datatable
     $('#dataTable-mensajes').DataTable();
+    
+    
+
 } );    
 
 
