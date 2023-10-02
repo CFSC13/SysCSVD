21c5ea2aee2aa5db821b605752f0a267f6098774
CFSC13 <christiansanabriapiano@gmail.com>
2023-10-02 19:53:31 -0300

    correccion sql bd #43

--- bd/#43_cambio_de_iconos_y_unificación_de_modulo_consultas.sql
+++ bd/#43_cambio_de_iconos_y_unificación_de_modulo_consultas.sql
@@ -3,16 +3,19 @@
 #Se eliminó el grupo reportes y también el item reportes estadísticos.
 DELETE FROM syscsvd_grupos_menu WHERE nombre_grupo = 'reportes';
 
-DELETE FROM syscsvd_items_interno WHERE url = 'stock_minimo.php';
 
 #se agregó el ítem reportes estadísticos al grupo consulta.
-INSERT INTO syscsvd_items_interno (nombre_item, url, id_grupo) VALUES ('Reportes', 'reportes.php', (select id_grupo from syscsvd_grupos_menu where nombre_grupo='consultas'))
+
+INSERT INTO syscsvd_items_interno (nombre_item, url, id_grupo) VALUES ('Reportes', 'reportes.php', (select id_grupo from syscsvd_grupos_menu where nombre_grupo='consultas'));
+
+INSERT INTO syscsvd_items_interno (nombre_item, url, id_grupo) VALUES ('Stock Mínimo', 'stock_minimo.php', (select id_grupo from syscsvd_grupos_menu where nombre_grupo='consultas'));
 
 #Se cambio el icono de grupo consultas
-UPDATE syscsvd_grupos_menu SET icono = 'fa fa-line-chart' WHERE nombre_grupo = 'consultas'
+
+UPDATE syscsvd_grupos_menu SET icono = 'fa fa-line-chart' WHERE nombre_grupo = 'consultas';
 
 
 #se agregó el ítem reportes estadísticos al grupo consulta.
-INSERT INTO syscsvd_items_interno (nombre_item, url, id_grupo) VALUES ('Atencion al cliente', 'attecliente_catalogo.php', (select id_grupo from syscsvd_grupos_menu where nombre_grupo='consultas'))
+INSERT INTO syscsvd_items_interno (nombre_item, url, id_grupo) VALUES ('Atencion al cliente', 'attecliente_catalogo.php', (select id_grupo from syscsvd_grupos_menu where nombre_grupo='consultas'));
 
 
